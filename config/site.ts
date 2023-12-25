cbf8ee73fb22b2cbfc2416870056b7c5a0682479
Çağkan Çağlar <cagkancaglar@hotmail.com>
2023-12-25 23:30:33 +0300

    Add: SiteConfig

---
+++ config/site.ts
@@ -0,0 +1,24 @@
+export interface SiteConfig {
+  siteName: string;
+  description: string;
+  currentlyAt: string;
+  socialLinks: {
+    github: string;
+    website: string;
+    linkedin: string;
+  };
+}
+
+const siteConfig = {
+  siteName: "Explorer",
+  description:
+    "A minimal and lovely travel  blog which shares experiences and citiest around the world!",
+  currentlyAt: "Istanbul",
+  socialLinks: {
+    github: "//github.com/cagkancaglar",
+    website: "//cagkancaglar.com",
+    linkedin: "//linkedin.com/cagkancaglar",
+  },
+};
+
+export default siteConfig;
