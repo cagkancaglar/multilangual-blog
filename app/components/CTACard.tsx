5502fbfedd586c36b50a9c16deb199ec3463a3bd
Çağkan Çağlar <cagkancaglar@hotmail.com>
2024-01-01 00:10:30 +0300

    Add: CTACard

---
+++ app/components/CTACard.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+
+const CTACard = () => {
+  return (
+    <div className="relative bg-slate-100 px-6 py-10 overflow-hidden rounded-md">
+      {/* Overlay */}
+      <div className="absolute inset-0 bg-gradient-to-br from-white/95 via-white/70 to-white/30 z-10" />
+      {/* Image */}
+      <Image
+        className="object-cover object-center"
+        fill
+        alt="CTA Card Image"
+        src="https://images.unsplash.com/photo-1585970480901-90d6bb2a48b5?ixid=MnwzODU2NTF8MHwxfHNlYXJjaHwxOHx8RWxlcGhhbnRzJTIwdGhhaWxhbmR8ZW58MHx8fHwxNjcwMzIyNzUx&ixlib=rb-4.0.3"
+      />
+      {/* Content Container */}
+      <div className="relative z-20">
+        <div className="text-lg font-medium">#exploretheworld</div>
+        <h3 className="mt-3 text-4xl font-semibold">
+          Explore the world with me!
+        </h3>
+        <p className="max-w-lg mt-2 text-lg">
+          Explore the world with me! I'm travelling around the world. I've
+          visited most of the great cities of and currently I'm travelling in
+          Join me!
+        </p>
+        <form className="mt-6 flex items-center gap-2 ">
+          <input
+            placeholder="Write your email."
+            className="px-4 py-3 text-base bg-white/80 rounded-md outline-none focus:ring-2 focus:ring-neutral-600 placeholder:text-sm"
+          />
+          <button className="px-4 py-3 rounded-md bg-neutral-900 text-neutral-200">
+            Sign Up
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default CTACard;
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import CTACard from "./components/CTACard";
 import PaddingContainer from "./components/PaddingContainer";
 import PostCard from "./components/PostCard";
 import PostList from "./components/PostList";
@@ -17,6 +18,7 @@ export default function Home() {
           posts={MOCK_POSTS.filter((_, i) => i > 3 && i < 6)}
           layout="vertical"
         />
+        <CTACard />
       </main>
     </PaddingContainer>
   );
